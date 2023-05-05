7f55642ac1ffb73505ed29af716eaa44bc776505
usman <usmanakhtar426@gmail.com>
2023-05-06 04:50:23 +0500

    fyp

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://ecommerce-market-project.herokuapp.com/api/v2";
+export const server = "https://mer-fyp.vercel.app/api/v2";
 
-export const backend_url = "https://ecommerce-market-project.herokuapp.com/";
+export const backend_url = "https://mer-fyp.vercel.app/";
